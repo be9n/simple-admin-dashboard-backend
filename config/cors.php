5517222c89f66811c17bfddb7270b36b54be71e7
Abdulrahmannas <a.nabbout@fromscratch-solutions.com>
2025-03-23 16:44:35 +0300

    Added frontend url to cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'https://simple-admin-dashboard-frontend-m14f.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
